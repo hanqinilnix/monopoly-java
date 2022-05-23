98caf54eff3cc3b703da5c40a92ccb723ea8fe6d
hanqinilnix <ohanqin@gmail.com>
2022-05-23 22:16:17 +0800

    Make attributes immutable

--- src/main/java/model/ChanceManager.java
+++ src/main/java/model/ChanceManager.java
@@ -19,22 +19,22 @@ import model.card.ReceiveCard;
 import model.spaces.PurchasableSpace;
 
 public class ChanceManager {
-    private static Card ADVANCE_TO_RED3 = new MoveToPropertyCard(String.format("Advance to %1$s", SpaceManager.RED3_NAME), (PurchasableSpace) SpaceManager.RED3);
-    private static Card ADVANCE_TO_NEAREST_UTILITY = new AdvanceToNearestCard("Advance token to nearest utility\nIf unowned, you may buy it from the bank\nIf owned, throw dice and pay owner a total 10 times the amount thrown", PurchasableType.UTILITY);
-    private static Card ADVANCE_TO_NEAREST_STATION = new AdvanceToNearestCard("Advance token to nearest station and pay owner twice the rental to which he/she is otherwise entitled\nIf station is unowned, you may buy it from the bank", PurchasableType.STATION);
-    private static Card ADVANCE_TO_NEAREST_STATION_SECOND = new AdvanceToNearestCard("Advance token to nearest station and pay owner twice the rental to which he/she is otherwise entitled\nIf station is unowned, you may buy it from the bank", PurchasableType.STATION);
-    private static Card GO_BACK_3 = new MoveXSpacesCard("Go back 3 spaces", -3);
-    private static Card PAY_CONSERVANCY_FEE = new PayBankCard("Pay conservancy fees of $150", 15);
-    private static Card BANK_PAYS_DIVIDEND = new ReceiveCard("Bank pays you dividend of $500", 50);
-    private static Card PAY_ALL_PLAYERS = new PayAllPlayersCard("You have been elected chairman of the board pay each player $500", 50);
-    private static Card RIDE_TO_STATION1 = new MoveToPropertyCard(String.format("Take a ride to %1$s\nIf you pass GO collect $200", SpaceManager.STATION1_NAME), (PurchasableSpace) SpaceManager.STATION1);
-    private static Card GET_OUT_OF_JAIL_FREE = new GetOutOfJailCard("Get out of jail free\nThis card may be kept until needed, or sold");
-    private static Card GENERAL_REPAIR = new PayPerHouseCard("Make general repairs on all your property\nFor each house pay $25\nfor each hotel $100", 25, 100);
-    private static Card ADVANCE_TO_BLUE2 = new MoveToPropertyCard(String.format("Take a walk on %1$s\nAdvance token to %2$s", SpaceManager.GREEN3_NAME, SpaceManager.BLUE2_NAME), (PurchasableSpace) SpaceManager.BLUE2);
-    private static Card ADVANCE_TO_PINK1 = new MoveToPropertyCard(String.format("Advance to %1$s\nIf you pass GO, collect $200", SpaceManager.PINK1_NAME), (PurchasableSpace) SpaceManager.PINK1);
-    private static Card CASH_IN_INVESTMENTS = new ReceiveCard("Cash in on investments\nCollect $150", 150);
-    private static Card GO_TO_JAIL = new GoToJailCard("Go directly to jail\nDo not pass go\nDo not collect $200");
-    private static Card ADVANCE_TO_GO = new AdvanceToGoCard("Advance to GO\n(Collect $200)");
+    private static final Card ADVANCE_TO_RED3 = new MoveToPropertyCard(String.format("Advance to %1$s", SpaceManager.RED3_NAME), (PurchasableSpace) SpaceManager.RED3);
+    private static final Card ADVANCE_TO_NEAREST_UTILITY = new AdvanceToNearestCard("Advance token to nearest utility\nIf unowned, you may buy it from the bank\nIf owned, throw dice and pay owner a total 10 times the amount thrown", PurchasableType.UTILITY);
+    private static final Card ADVANCE_TO_NEAREST_STATION = new AdvanceToNearestCard("Advance token to nearest station and pay owner twice the rental to which he/she is otherwise entitled\nIf station is unowned, you may buy it from the bank", PurchasableType.STATION);
+    private static final Card ADVANCE_TO_NEAREST_STATION_SECOND = new AdvanceToNearestCard("Advance token to nearest station and pay owner twice the rental to which he/she is otherwise entitled\nIf station is unowned, you may buy it from the bank", PurchasableType.STATION);
+    private static final Card GO_BACK_3 = new MoveXSpacesCard("Go back 3 spaces", -3);
+    private static final Card PAY_CONSERVANCY_FEE = new PayBankCard("Pay conservancy fees of $150", 15);
+    private static final Card BANK_PAYS_DIVIDEND = new ReceiveCard("Bank pays you dividend of $500", 50);
+    private static final Card PAY_ALL_PLAYERS = new PayAllPlayersCard("You have been elected chairman of the board pay each player $500", 50);
+    private static final Card RIDE_TO_STATION1 = new MoveToPropertyCard(String.format("Take a ride to %1$s\nIf you pass GO collect $200", SpaceManager.STATION1_NAME), (PurchasableSpace) SpaceManager.STATION1);
+    private static final Card GET_OUT_OF_JAIL_FREE = new GetOutOfJailCard("Get out of jail free\nThis card may be kept until needed, or sold");
+    private static final Card GENERAL_REPAIR = new PayPerHouseCard("Make general repairs on all your property\nFor each house pay $25\nfor each hotel $100", 25, 100);
+    private static final Card ADVANCE_TO_BLUE2 = new MoveToPropertyCard(String.format("Take a walk on %1$s\nAdvance token to %2$s", SpaceManager.GREEN3_NAME, SpaceManager.BLUE2_NAME), (PurchasableSpace) SpaceManager.BLUE2);
+    private static final Card ADVANCE_TO_PINK1 = new MoveToPropertyCard(String.format("Advance to %1$s\nIf you pass GO, collect $200", SpaceManager.PINK1_NAME), (PurchasableSpace) SpaceManager.PINK1);
+    private static final Card CASH_IN_INVESTMENTS = new ReceiveCard("Cash in on investments\nCollect $150", 150);
+    private static final Card GO_TO_JAIL = new GoToJailCard("Go directly to jail\nDo not pass go\nDo not collect $200");
+    private static final Card ADVANCE_TO_GO = new AdvanceToGoCard("Advance to GO\n(Collect $200)");
 
     private LinkedList<Card> chanceCards;
     private boolean drawnGetOutOfJail = false;
--- src/main/java/model/CommunityChestManager.java
+++ src/main/java/model/CommunityChestManager.java
@@ -14,22 +14,22 @@ import model.card.PayPerHouseCard;
 import model.card.ReceiveCard;
 
 public class CommunityChestManager {
-    private static Card INCOME_TAX_REFUND = new ReceiveCard("Income Tax Refund\nCollect $20", 20);
-    private static Card GIC_ISSUES_DIVIDENDS = new ReceiveCard("GIC issues dividends\nCollect $100", 100);
-    private static Card ADVANCE_TO_GO = new AdvanceToGoCard("Advance to GO\n(Collect $200)");
-    private static Card INHERITANCE = new ReceiveCard("You inherit $100", 100);
-    private static Card DOCTOR_FEE = new PayBankCard("Doctor's fee\nPay $50", 50);
-    private static Card GRAND_OPERA_OPENING = new CollectFromPlayersCard("Grand Opera Opening\nCollect $50 from every player\nFor opening night seats", 50);
-    private static Card LIFE_INSURANCE = new ReceiveCard("Life insurance matures\nCollect $100", 100);
-    private static Card PAY_HOSPITAL = new PayBankCard("Pay hospital $100", 100);
-    private static Card GO_TO_JAIL = new GoToJailCard("Go to jail\nGo Directly to jail\nDo no pass GO\nDo not collect $200");
-    private static Card BANK_ERROR = new ReceiveCard("Bank error in your favour\nCollect $200", 200);
-    private static Card STREET_REPAIRS = new PayPerHouseCard("You are assessed for street repairs\n$40 per house\n$115 per hotel", 40, 115);
-    private static Card GET_OUT_OF_JAIL_FREE =new GetOutOfJailCard("Get out of jail free\nThis card maybe kept until needed\nor sold");
-    private static Card SECOND_PRIZE = new ReceiveCard("You have won second prize in a lucky draw\nCollect 10", 10);
-    private static Card SCHOOL_FEES = new PayBankCard("Pay school fees of $150", 150);
-    private static Card SALE_STOCK = new ReceiveCard("From sale fo stock you get $45", 45);
-    private static Card SERVICES = new ReceiveCard("Receive for services $250", 250);
+    private static final Card INCOME_TAX_REFUND = new ReceiveCard("Income Tax Refund\nCollect $20", 20);
+    private static final Card GIC_ISSUES_DIVIDENDS = new ReceiveCard("GIC issues dividends\nCollect $100", 100);
+    private static final Card ADVANCE_TO_GO = new AdvanceToGoCard("Advance to GO\n(Collect $200)");
+    private static final Card INHERITANCE = new ReceiveCard("You inherit $100", 100);
+    private static final Card DOCTOR_FEE = new PayBankCard("Doctor's fee\nPay $50", 50);
+    private static final Card GRAND_OPERA_OPENING = new CollectFromPlayersCard("Grand Opera Opening\nCollect $50 from every player\nFor opening night seats", 50);
+    private static final Card LIFE_INSURANCE = new ReceiveCard("Life insurance matures\nCollect $100", 100);
+    private static final Card PAY_HOSPITAL = new PayBankCard("Pay hospital $100", 100);
+    private static final Card GO_TO_JAIL = new GoToJailCard("Go to jail\nGo Directly to jail\nDo no pass GO\nDo not collect $200");
+    private static final Card BANK_ERROR = new ReceiveCard("Bank error in your favour\nCollect $200", 200);
+    private static final Card STREET_REPAIRS = new PayPerHouseCard("You are assessed for street repairs\n$40 per house\n$115 per hotel", 40, 115);
+    private static final Card GET_OUT_OF_JAIL_FREE =new GetOutOfJailCard("Get out of jail free\nThis card maybe kept until needed\nor sold");
+    private static final Card SECOND_PRIZE = new ReceiveCard("You have won second prize in a lucky draw\nCollect 10", 10);
+    private static final Card SCHOOL_FEES = new PayBankCard("Pay school fees of $150", 150);
+    private static final Card SALE_STOCK = new ReceiveCard("From sale fo stock you get $45", 45);
+    private static final Card SERVICES = new ReceiveCard("Receive for services $250", 250);
 
     private LinkedList<Card> communityChestCards;
     private boolean drawnGetOutOfJail = false;
